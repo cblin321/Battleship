928051d7fa833ce05c95a1d22218b712d94ece78
Chris Lin <chrislin90210@gmail.com>
2024-12-24 21:09:24 -0600

    finished 2 tests

--- Gameboard.js
+++ Gameboard.js
@@ -13,50 +13,62 @@ class Gameboard {
      * @param {Ship} ship ship to place, should have no damage 
      * @param {Array} coords expects in (x, y) 
      * @param {String} orientation indicates where the ship's length will point  
-     */
+     * @returns true if the ship was placed, false otherwise
+    */
     placeShip(ship, coords, orientation) {
-        if (!this.#isInBounds(ship, coords, orientation) && this.#inBoard(coords))
-            throw new Error("Ship out of bounds")
+        if (!this.#isInBounds(ship, coords, orientation) || !this.#inBoard(coords))
+            return false
 
         if (this.#willOverlap(ship, coords, orientation)) {
-            throw new Error("Ship will overlap with other ships")
+            return false
         }
+
+        if (this.ships.includes(ship))
+            return false
         
         this.ships.push(ship)
         this.#fillGridWithShip(ship, coords, orientation)
         this.#currId++
+        return true
     }
 
     #fillGridWithShip(ship, coords, orientation) {
         
         if (orientation === "left")
-            this.ship_grid[coords[0]].fill(this.#currId, coords[1] - ship.length, coords[1])
+            this.ship_grid[coords[1]].fill(this.#currId, coords[0] - (ship.length - 1), coords[0] + 1)
         
         if (orientation === "right")
-            this.ship_grid[coords[0]].fill(this.#currId, coords[1], coords[1] + ship.length)
+            this.ship_grid[coords[1]].fill(this.#currId, coords[0], coords[0] + ship.length)
         
         if (orientation === "up") {
-            for (let i = coords[1] - ship.length; i < coords[1]; i++)
+            for (let i = coords[1] - (ship.length - 1); i <= coords[1]; i++)
                 this.ship_grid[i][coords[0]] = this.#currId;
         }
 
         if (orientation === "down") {
-            for (let i = coords[1] + ship.length; i >= coords[1] ; i--)
+            for (let i = coords[1] + (ship.length - 1); i >= coords[1] ; i--)
                 this.ship_grid[i][coords[0]] = this.#currId;
         }
     }
 
+    /**
+     * 
+     * @param {*} ship 
+     * @param {*} coords 
+     * @param {*} orientation 
+     * @returns 
+     */
     #isInBounds(ship, coords, orientation) {
         //TODO overlapping ships
         switch(orientation) {
             case "left":
-                return 0 > coords[0] - ship.length 
+                return 0 <= coords[0] - (ship.length - 1) 
             case "right":
-                return this.ship_grid.length > coords[0] + ship.length
+                return this.ship_grid.length >= coords[0] + (ship.length - 1)
             case "up":
-                0 > coords[1] - ship.length
+                0 <= coords[1] - (ship.length - 1)
             case "down":
-                return this.ship_grid.length > coords[1] + ship.length
+                return this.ship_grid.length >= coords[1] + (ship.length - 1)
         }
     } 
 
--- Gameboard.test.js
+++ Gameboard.test.js
@@ -1,5 +1,6 @@
 import Gameboard from "./Gameboard.js"
-import {describe, expect, test} from '@jest/globals'
+import Ship from "./Ship.js"
+import {beforeEach, describe, expect, test} from '@jest/globals'
 
 describe("constructor & existence tester", () => {
     test("gameboard existence", () => expect(Gameboard).toBeTruthy())
@@ -14,17 +15,80 @@ describe("constructor & existence tester", () => {
 })
 
 describe("placeShip tester", () => {
-    //oob (all orientations)
-    //overlapping (all orientations)
-    //out of board but contains pts in board (all orientaitons)
+    
+    let ship = new Ship(4)
+    let ship2 = new Ship(5)
+    let ship3 = new Ship(3)
+    let ship4 = new Ship(2)
+    let board = new Gameboard(10)
+    
+    beforeEach(()=> {
+        ship = new Ship(4)
+        ship2 = new Ship(5)
+        ship3 = new Ship(3)
+        ship4 = new Ship(2)
+        board = new Gameboard(10)
+    })
+    
     //working case (all orientations)
-
-    test("working case", () => {
+    test("working tester", () => expect((() => {
+        //TODO refactor
+        const place1 = board.placeShip(ship, [3, 4], "down") //3, 4 - 3, 8
+        const place2 = board.placeShip(ship2, [2, 6], "up")
+        const place3 = board.placeShip(ship3, [3, 8], "left")
+        const place4 = board.placeShip(ship4, [0, 0], "right")
+        return [place1, place2, place3, place4, board.ship_grid, board.shots, board.ships] 
+    })()).toEqual((() => {
+        const expectedGrid = Array(10).fill().map(() => Array(10).fill(false))
+        for (let i = 4; i < 8; i++) 
+            expectedGrid[i][3] = 0 
         
+        for (let i = 2; i < 7; i++) 
+            expectedGrid[i][2] = 1 
+        
+        for (let i = 1; i < 4; i++) 
+            expectedGrid[8][i] = 2
+        
+        for (let i = 0; i < 2; i++) 
+            expectedGrid[0][i] = 3 
+        
+        const expectedShots = Array(10).fill().map(() => Array(10).fill(-1))
+        
+        const expectedShips = [ship, ship2, ship3, ship4]
+        
+        return [true, true, true, true, expectedGrid, expectedShots, expectedShips]
+    })()))
+    
+    //starting point oob (all orientations)
+    test("starting coordinates out of bounds (negative x)", () => expect(() => board.placeShip(ship, [-1, 0], "down").toThrow("Ship out of bounds")).toThrow("Ship out of bounds"))
+    test("starting coordinates out of bounds (large x)", () => expect(() => board.placeShip(ship2, [10, 6], "up")).toThrow("Ship out of bounds"))
+    test("starting coordinates out of bounds (negative y)", () => expect(() => board.placeShip(ship3, [3, -3], "left")).toThrow("Ship out of bounds"))
+    test("starting coordinates out of bounds (large x)", () => expect(() => board.placeShip(ship4, [0, 10], "right")).toThrow("Ship out of bounds"))
+    
+    //overlapping (all orientations)
+    test("overlapping ships (down orientation)", () => {
+        const existingShip = new Ship(10)
+        board.placeShip(existingShip, [5, 0], "right")
+        board.placeShip(ship, [8, 2])
+    }).toThrow("Ship will overlap with other ships")
+
+    test("overlapping ships (up orientation)", () => {
+        const existingShip = new Ship(10)
+        board.placeShip(existingShip, [5, 0], "right")
     })
-    test("starting coordinates out of bounds", () => {
+
+    test("overlapping ships (left orientation)", () => {
+        const existingShip = new Ship(10)
+        board.placeShip(existingShip, [5, 0], "right")
+    })
+
+    test("overlapping ships (right orientation)", () => {
+        const existingShip = new Ship(10)
+        board.placeShip(existingShip, [5, 0], "right")
+    })
+    
+    //out of board but contains pts in board (all orientaitons)
+    test("oob but contains pts in board (up orientation)", () => {
 
     })
-    test("overlapping ships",)
-    test("oob but contains pts in board",)
 })
